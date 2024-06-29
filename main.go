3536ee14fc7cc13b2f8f4d3d9987de2f2939563e
Jade Vasconcellos <jdvscnclls@gmail.com>
2024-06-29 18:44:38 -0300

    Utilizando interfaces

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"banco/clientes"
 	"banco/contas"
 	"fmt"
 )
 
+func pagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
-	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
-	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
+	contaDoDenis := contas.ContaPoupanca{}
+	contaDoDenis.Depositar(100)
+	pagarBoleto(&contaDoDenis, 60)
 
-	status := contaDaSilvia.Transferir(200, &contaDoGustavo)
+	fmt.Println(contaDoDenis.ObterSaldo())
+	fmt.Println(clientes.Titular{"Denis", "123.3459.394", "Desenvolvedor"})
 
-	fmt.Println(status)
-	fmt.Println(contaDoGustavo)
-	fmt.Println(contaDaSilvia)
-
+	contaDaLuisa := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	pagarBoleto(&contaDaLuisa, 400)
+	fmt.Println(contaDaLuisa.ObterSaldo())
 }
