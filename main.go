1811002c4c2b16e3141efeac567723c9294a8624
Jade Vasconcellos <jdvscnclls@gmail.com>
2024-06-29 10:53:37 -0300

    adicionando ponteiros e pacotes

--- main.go
+++ main.go
@@ -9,9 +9,43 @@ type contaCorrente struct {
 	saldo         float64
 }
 
+func (c *contaCorrente) sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *contaCorrente) depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso", c.saldo
+	} else {
+		return "Valor inválido", c.saldo
+	}
+}
+
+func (c *contaCorrente) transferir(valorDaTransferencia float64, contaDestino *contaCorrente) bool {
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
-	contaDaJade := contaCorrente{"Jade", 589, 123456, 125.5}
-	contaDaBruna := contaCorrente{"Bruna", 222, 11112, 200}
-	fmt.Println(contaDaJade)
-	fmt.Println(contaDaBruna)
+	contaDaSilvia := contaCorrente{titular: "Silvia", saldo: 300}
+	contaDoGustavo := contaCorrente{titular: "Gustavo", saldo: 100}
+
+	status := contaDaSilvia.transferir(200, &contaDoGustavo)
+
+	fmt.Println(status)
+	fmt.Println(contaDoGustavo)
+	fmt.Println(contaDaSilvia)
+
 }
